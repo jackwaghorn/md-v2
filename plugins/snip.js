5b438c932ced377c19a39b8bc498843650de01c9
Jack Waghorn <jackwaghorn@Jacks-MacBook-Pro.local>
2022-12-06 19:01:35 +0000

    upte'

--- plugins/snip.js
+++
@@ -1,5 +0,0 @@
-import VueSnip from 'vue-snip'
-
-export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.vueApp.use(VueSnip)
-})
--- plugins/swiper.js
+++
@@ -1,5 +0,0 @@
-import SwiperCore, { Pagination, Autoplay } from 'swiper'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(new SwiperCore)
-})
